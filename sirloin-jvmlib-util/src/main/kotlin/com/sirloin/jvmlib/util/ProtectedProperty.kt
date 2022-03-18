36259fbb9228536bbab2cb78a40eaf429830988b
Hwan Jo <nimbusob@gmail.com>
2022-03-19 08:18:25 +0900

    #2 Fix ProtectedProperty to immutable

--- sirloin-jvmlib-util/src/main/kotlin/com/sirloin/jvmlib/util/ProtectedProperty.kt
+++ sirloin-jvmlib-util/src/main/kotlin/com/sirloin/jvmlib/util/ProtectedProperty.kt
@@ -8,12 +8,10 @@ package com.sirloin.jvmlib.util
  * Wrap your sensitive values, password, access token, etc. with this class to prevent it being
  * exposed to outside world via [toString] method, especially those properties of Kotlin data class.
  *
- * `value` is declared as `var` for immediate removal of sensitive information from memory if required.
- *
  * @since 2022-02-14
  */
 class ProtectedProperty<T> @JvmOverloads constructor(
-    var value: T,
+    val value: T,
     private val scrubText: String = DEFAULT_SCRUB_TEXT
 ) {
     override fun toString() = scrubText
