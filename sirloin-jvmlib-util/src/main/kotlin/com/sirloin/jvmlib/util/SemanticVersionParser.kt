44ce7a93ac4ce428a6291c0cc28d1d3007633e8d
yoonho <yoonho2015@gmail.com>
2022-05-20 19:03:50 +0900

    코드리뷰 수정사항 반영

--- sirloin-jvmlib-util/src/main/kotlin/com/sirloin/jvmlib/util/SemanticVersionParser.kt
+++ sirloin-jvmlib-util/src/main/kotlin/com/sirloin/jvmlib/util/SemanticVersionParser.kt
@@ -13,14 +13,13 @@ object SemanticVersionParser {
 
     // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
     private const val PATTERN_SEMVER =
-        "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)$?" +
+        "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)" +
                 "(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?" +
-                "(?:\\+([0-9a-zA-Z-_]+(?:\\.[0-9a-zA-Z-]+)*))?$"
+                "(?:\\+([0-9a-zA-Z-_]+(?:\\.[0-9a-zA-Z-]+)*))?"
 
     fun toSemanticVersion(semVer: String): SemanticVersion? =
         Regex(PATTERN_SEMVER)
-            .find(semVer)?.let { matchResult ->
-                matchResult.destructured.let { (major, minor, patch, preRelease, build) ->
+            .matchEntire(semVer)?.destructured?.let { (major, minor, patch, preRelease, build) ->
                     SemanticVersion(
                         major = major.toInt(),
                         minor = minor.toInt(),
@@ -28,7 +27,6 @@ object SemanticVersionParser {
                         preRelease = preRelease,
                         build = build
                     )
-                }
             }
 
     fun isMatchesToSemanticVersion(semVer: String): Boolean =
